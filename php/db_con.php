5792ce07fa831776b749a65d594ac0e662afdf00
JungJung <77655831+wjdektjd0878@users.noreply.github.com>
2021-09-09 14:37:28 +0900

    Update db_con.php

--- php/db_con.php
+++ php/db_con.php
@@ -1,10 +1,10 @@
 <?php
     // mysqli
-    $db_host = "localhost:3306";
+    $db_host = "192.168.1.134";
     $db_user = "itbank";
     $db_pass = "itbank";
     $db_name = "damem";
-    $conn = mysqli_connect('localhost:3306','root','','damem');
+    $conn = mysqli_connect('192.168.1.134','itbank','itbank','damem');
     /*
     if (!$conn) {
         echo "<script>alert(\"DB Connection False\");</script>";
