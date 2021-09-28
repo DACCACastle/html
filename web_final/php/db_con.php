59e37632b2779b58e507ddf5848900370bd06146
susuzy <csg27@naver.com>
2021-09-28 11:39:55 +0900

    Add files via upload

---
+++ web_final/php/db_con.php
@@ -0,0 +1,17 @@
+<?php
+    // mysqli
+    $db_host = "dbaddress";
+    $db_user = "itbank";
+    $db_pass = "itbank";
+    $db_name = "damem";
+    $conn = mysqli_connect('dbaddress','itbank','itbank','damem');
+    /*
+    if (!$conn) {
+        echo "<script>alert(\"DB Connection False\");</script>";
+    }
+    else {
+        echo "<script>alert(\"DB Connection Success\");</script>";
+    }
+    */
+?> 
+
