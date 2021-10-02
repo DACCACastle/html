9960882e9211c2d81e1c3bc4a35639f2a8fbac59
JungJung <77655831+wjdektjd0878@users.noreply.github.com>
2021-10-02 15:25:59 +0900

    Update db_con.php

--- php/db_con.php
+++ php/db_con.php
@@ -5,13 +5,38 @@
     $db_pass = "asdASD123";
     $db_name = "damem";
     $conn = mysqli_connect('dbaddress','asdASD123','asdASD123','damem');
-    /*
-    if (!$conn) {
-        echo "<script>alert(\"DB Connection False\");</script>";
-    }
-    else {
-        echo "<script>alert(\"DB Connection Success\");</script>";
-    }
-    */
-?> 
 
+    $result_exist_member = mysql_query("SHOW TABLES LIKE 'member'");
+
+    $row_exist_member = mysql_fetch_array($result_exist_member); 
+
+    if($row_exist_member == true){			
+        echo "";
+    } 
+
+    else { 
+
+        $sql1 = "create table member(No int auto_increment primary key, 
+        ID varchar(50) not null, 
+        PW varchar(100) not null, 
+        phone varchar(50) not null, 
+        Mail varchar(50) not null)";
+
+    }
+
+    $result_exist_board = mysql_query("SHOW TABLES LIKE 'board'");
+
+    $row_exist_board = mysql_fetch_array($result_exist_board); 
+
+    if($row_exist_board == true){
+        echo "";
+    } 
+
+    else { 
+        $sql2 = "create table board(No int auto_increment primary key, 
+        Title varchar(50) not null, 
+        Content varchar(500) not null, 
+        Userid varchar(50) not null, 
+        Date varchar(50) not null)";
+    }
+?>
