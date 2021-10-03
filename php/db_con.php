c78172c1c3e653f460dcd9a341503832d56d9f8d
JungJung <77655831+wjdektjd0878@users.noreply.github.com>
2021-10-03 19:19:47 +0900

    Update db_con.php

--- php/db_con.php
+++ php/db_con.php
@@ -5,12 +5,8 @@
     $db_pass = "asdASD123";
     $db_name = "damem";
     $conn = mysqli_connect('dbaddress','asdASD123','asdASD123','damem');
-    /*
-    if (!$conn) {
-        echo "<script>alert(\"DB Connection False\");</script>";
-    }
-    else {
-        echo "<script>alert(\"DB Connection Success\");</script>";
-    }
-    */
+    $sql ="create table member(No int auto_increment primary key, ID varchar(50) not null, PW varchar(100) not null, phone varchar(50) not null, Mail varchar(50) not null);";
+
+    $result=mysqli_query($conn,$sql);
+    mysqli_close($conn);
 ?> 
