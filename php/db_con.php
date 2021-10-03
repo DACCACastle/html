3ac4954a1aaea0dea0de19d2cbc6d77d8f4613b6
JungJung <77655831+wjdektjd0878@users.noreply.github.com>
2021-10-03 21:14:27 +0900

    Update db_con.php

--- php/db_con.php
+++ php/db_con.php
@@ -8,5 +8,4 @@
     $sql ="create table member(No int auto_increment primary key, ID varchar(50) not null, PW varchar(100) not null, phone varchar(50) not null, Mail varchar(50) not null);";
 
     $result=mysqli_query($conn,$sql);
-    mysqli_close($conn);
 ?> 
